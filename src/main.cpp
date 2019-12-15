02616016885b3158850552ec0feb4c78f0338705
Chris Searle <chris@chrissearle.org>
2019-12-15 20:27:54 +0100

    Initial - as yet untested

---
+++ src/main.cpp
@@ -0,0 +1,153 @@
+#include <Arduino.h>
+
+#include <Adafruit_NeoPixel.h>
+#include <Servo.h>
+
+// Light strip
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(2, 11, NEO_GRB + NEO_KHZ800);
+unsigned int colorChange = 500;
+unsigned long lastLoopStrip = 0;
+
+// Motors
+unsigned int motorInA = 3;
+unsigned int motorInB = 5;
+unsigned long lastLoopMotor = 0;
+unsigned int motorThreshold = 300;
+bool motorRunning = false;
+
+// Servos
+unsigned int lowerServoPin = 6;
+Servo lowerServo;
+
+// LDR
+unsigned int lightThreshold = 800;
+unsigned int lightSensorPin = 2;
+
+// Colours
+uint32_t red = strip.Color(153, 0, 0);
+uint32_t green = strip.Color(0, 153, 0);
+uint32_t blue = strip.Color(93, 173, 226);
+uint32_t yellow = strip.Color(244, 208, 63);
+
+// house lights - small, curved, carousel, large
+unsigned int lightPins[] = {2, 7, 8, 10};
+unsigned long lastLoopLights[] = {0, 0, 0, 0};
+unsigned int lightThresholds[] = {400, 750, 250, 360};
+
+void motorOn(PinStatus direction, int speed)
+{
+  digitalWrite(motorInA, direction);
+  analogWrite(motorInB, speed);
+  motorRunning = true;
+}
+
+void motorOff()
+{
+  digitalWrite(motorInA, LOW);
+  digitalWrite(motorInB, LOW);
+  motorRunning = false;
+}
+
+void setup()
+{
+  strip.begin();
+
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(lightPins[i], OUTPUT);
+  }
+
+  pinMode(motorInA, OUTPUT);
+  pinMode(motorInB, OUTPUT);
+
+  motorOff();
+
+  lowerServo.attach(lowerServoPin);
+}
+
+void houseLightsOff()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    digitalWrite(lightPins[i], LOW);
+  }
+}
+
+void setColors(uint32_t one, uint32_t two)
+{
+  unsigned long current = millis();
+
+  if ((current - lastLoopStrip) >= colorChange)
+  {
+    if (random(100) % 2 == 0)
+    {
+      strip.setPixelColor(0, one);
+      strip.setPixelColor(1, two);
+    }
+    else
+    {
+      strip.setPixelColor(0, two);
+      strip.setPixelColor(1, one);
+    }
+
+    strip.show();
+
+    lastLoopStrip = current;
+  }
+}
+
+void setMotors()
+{
+  unsigned long current = millis();
+
+  if ((current - lastLoopMotor) > motorThreshold)
+  {
+    if (random(100) % 2 == 0)
+    {
+      bool direction = (random(100) % 2) == 0;
+      motorOn(direction ? LOW : HIGH, random(255));
+    }
+  }
+}
+
+void checkHouseLights()
+{
+  unsigned long current = millis();
+
+  for (int i = 0; i < 4; i++)
+  {
+    if (lastLoopLights[i] - current >= lightThresholds[i])
+    {
+      PinStatus pinState = digitalRead(lightPins[i]);
+      digitalWrite(lightPins[i], pinState == LOW ? HIGH : LOW);
+      lastLoopLights[i] = current;
+    }
+  }
+}
+
+void evening()
+{
+  setColors(red, green);
+  checkHouseLights();
+}
+
+void daytime()
+{
+  setColors(blue, yellow);
+  houseLightsOff();
+  motorOff();
+}
+
+void loop()
+{
+  lowerServo.write(90);
+
+  if ((analogRead(lightSensorPin)) > lightThreshold)
+  {
+    evening();
+  }
+  else
+  {
+    daytime();
+  }
+}
